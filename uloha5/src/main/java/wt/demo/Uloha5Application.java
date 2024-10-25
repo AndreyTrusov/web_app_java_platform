6a2a29e86271667f2249a7e73cff19f1b44687f3
aatrusov <trusovandrey.aleks@gmail.com>
2024-10-25 12:02:38 +0200

    created base to start implementing home work

---
+++ uloha5/src/main/java/wt/demo/Uloha5Application.java
@@ -0,0 +1,13 @@
+package wt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uloha5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uloha5Application.class, args);
+	}
+
+}
